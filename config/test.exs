f07acb411e1e44e79d98d7226f25d559cb2ce016
Derrick Zhang <derrick.m.zhang@gmail.com>
2019-09-25 18:08:47 +0800

    Setup CI workflow

--- config/test.exs
+++ config/test.exs
@@ -1,11 +1,8 @@
 import Config
 
 config :bugle, Bugle.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "bugle_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  url: System.get_env("DATABASE_URL") || "ecto://postgres:postgres@localhost/bugle_test"
 
 config :bugle, BugleWeb.Endpoint,
   http: [port: 4002],
